3cb9b736e0d15db2054b22086c68e4b4078f14b1
Rishit Dagli <rishit.dagli@gmail.com>
2023-04-14 20:54:41 -0400

    Update tfrecords path

--- cppe5/download_tfrecords.py
+++ cppe5/download_tfrecords.py
@@ -14,7 +14,7 @@ def download_tfrecords(dir_name="tfrecords") -> None:
         os.mkdir(dir_name)
     os.chdir(dir_name)
     gdown.download(
-        "https://drive.google.com/uc?id=1jBHxybNWx4uhLxWxyguHpmjzz73YMQdG", quiet=False
+        "https://drive.google.com/uc?id=1kI1cemIYC5MOgwkP-2-jeznbSSLEHJsp", quiet=False
     )
     os.system("tar -xf tfrecords.tar.gz")
     os.remove("tfrecords.tar.gz")
